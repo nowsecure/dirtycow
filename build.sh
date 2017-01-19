eb87b43a5d86146cf433c13ad763dd69d74bb1e7
Sergi Àlvarez i Capilla <pancake@nowsecure.com>
2017-01-19 14:09:25 +0100

    Replicate comment in code for the build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,10 @@
 # script to build dirtycow on Termux
 #
 export T=/data/data/com.termux/files/usr
+if [ -d "$T" ]; then
+	echo "This script must be run in a native Android inside the Termux shell"
+	exit 1
+fi
 gcc -o io_dirtycow.so io_dirtycow.c -I $T/include/libr -L $T/lib -lr_util -lr_io -shared -fPIC
 mkdir -p ~/.config/radare2/plugins
 cp -f io_dirtycow.so ~/.config/radare2/plugins
